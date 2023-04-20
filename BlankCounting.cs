bfc7f60f566a19c47d514d1434a5e7b46fe0d8ce
Lee Nhung <130581573+immaFrogUwU@users.noreply.github.com>
2023-04-20 17:51:32 +0700

    Update BlankCounting.cs

--- BlankCounting.cs
+++ BlankCounting.cs
@@ -2,24 +2,79 @@ using System;
 using System.IO;
 using static System.Console;
 
-namespace HomeWork_BlankCounting
+namespace ThongTinHocVien
 {
-    public class BlankCounting
+
+    public struct SinhVien
     {
-    
-        public void Main(string[] args)
-        {
-               
-        }
-        public void Count_Variable()
-        {
-            int biendem;
-            string stringtong;
-            for( int i = 0; i < stringtong.Length; i++)
-            {
-                if 
-            }
-        }
-        
+        public string HoTen;
+        public string GioiTinh;
+        public DateTime NgaySinh;
+        public string TruongHoc;
+        public string NoiO;
+        public string QueQuan;
     }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            OutputEncoding = Encoding.UTF8;
+            SinhVien[] DanhSachSinhVien =
+            {
+            new SinhVien { HoTen = "Lê Hồng Nhung", GioiTinh = "Nữ", NgaySinh = new DateTime(2004, 8, 25), TruongHoc = "NEU", NoiO = "Chung cư hoàng huy", QueQuan = "Thanh Hóa" },
+            new SinhVien { HoTen = "Trần Minh Đức", GioiTinh = "Nam", NgaySinh = new DateTime(2002, 2, 13), TruongHoc = "Trường Kinh doanh Công nghệ", NoiO = "Hà Nội", QueQuan = "Hải Dương" },
+            new SinhVien { HoTen = "Vũ Đức Nam", GioiTinh = "Nam", NgaySinh = new DateTime(2003, 5, 10), TruongHoc = "Cao đẳng fpt ponitechnich", NoiO = "Hà Nội", QueQuan = "Hải Dương" },
+            new SinhVien { HoTen = "Lê Đinh Hùng Anh", GioiTinh = "Nam", NgaySinh = new DateTime(2003, 4, 27), TruongHoc = "Lam Sơn", NoiO = "ktx Bách Khoa", QueQuan = "Thanh hóa" },
+            new SinhVien { HoTen = "Nguyễn Trần Minh", GioiTinh = "Nam", NgaySinh = new DateTime(2003, 3, 25), TruongHoc = "HUST", NoiO = "250 minh khai", QueQuan = "Thái Bình" },
+            new SinhVien { HoTen = "Lê Xuân Phúc", GioiTinh = "Nam", NgaySinh = new DateTime(2004, 2, 17), TruongHoc = "HUST", NoiO = "Hà Nội", QueQuan = "Thanh Hóa" },
+            new SinhVien { HoTen = "Hòang Trung Khải", GioiTinh = "Nam", NgaySinh = new DateTime(2004, 10, 9), TruongHoc = "HUST", NoiO = "hoàng mai", QueQuan = " thanh hóa" },
+            new SinhVien { HoTen = "Bùi Quốc Bảo", GioiTinh = "Nam", NgaySinh = new DateTime(2004, 8, 14), TruongHoc = "HUST", NoiO = "đống đa", QueQuan = "thanh hóa" },
+            new SinhVien { HoTen = "Nguyễn ngọc châu", GioiTinh = "Nam", NgaySinh = new DateTime(2001, 1, 17), TruongHoc = "đại học cần thơ", NoiO = "cần thơ", QueQuan = "sóc trăng" },
+            new SinhVien { HoTen = "Nguyễn công nghĩa", GioiTinh = "Nam", NgaySinh = new DateTime(2002, 5, 3), TruongHoc = "hb âcdemy", NoiO = "hà nội", QueQuan = "hà nội" },
+            new SinhVien { HoTen = "Nguyễn chiều dương", GioiTinh = "Nam", NgaySinh = new DateTime(2004, 11, 9), TruongHoc = "greenwich", NoiO = "Vinhome", QueQuan = "Hà nội" },
+            new SinhVien { HoTen = "Đặng thành long", GioiTinh = "Nam", NgaySinh = new DateTime(1999, 7, 16), TruongHoc = "hb academy", NoiO = "hà nội", QueQuan = "Hà nam" },
+
+            };
+
+            WriteLine("Thông tin học viên :");
+            WriteLine("**********---************)");
+            for (int i = 0; i < DanhSachSinhVien.Length; i++)
+            {
+                
+                Console.WriteLine("Học viên thứ " + (i + 1) + ":");
+                Console.WriteLine("Họ tên: " + DanhSachSinhVien[i].HoTen);
+                Console.WriteLine("Giới tính: " + DanhSachSinhVien[i].GioiTinh);
+                Console.WriteLine("Ngày sinh: " + DanhSachSinhVien[i].NgaySinh.ToString("dd/MM/yyyy"));
+                Console.WriteLine("Trường học: " + DanhSachSinhVien[i].TruongHoc);
+                Console.WriteLine("Địa chỉ: " + DanhSachSinhVien[i].NoiO);
+                Console.WriteLine("Quê quán: " + DanhSachSinhVien[i].QueQuan);
+                Console.WriteLine();
+                
+            }
+            for (int i = 0; i < DanhSachSinhVien.Length; i++)
+            {               
+                string HoTen = DanhSachSinhVien[i].HoTen;
+                WriteLine("Số khoảng trắng trong tên học viên " + (i + 1) + " là " + BienDem(HoTen));
+            
+            }
+                                 
+         
+        }
+        public static int BienDem(string stringtong)
+        {
+            int biendem = 0;
+            string stringmain;
+            for (int i = 0; i < stringtong.Length; i++)
+            {
+                stringmain = stringtong.Substring(i, 1);
+                if (stringmain == " ")
+                {
+                    biendem++;
+                }                   
+            }
+            return biendem;
+        }
+
+        }
 }
