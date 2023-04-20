64e4de560b64adda6d47d194fc45f39b2513198a
Lee Nhung <130581573+immaFrogUwU@users.noreply.github.com>
2023-04-20 10:46:16 +0700

    Create BlankCounting.cs

---
+++ BlankCounting.cs
@@ -0,0 +1,25 @@
+using System;
+using System.IO;
+using static System.Console;
+
+namespace HomeWork_BlankCounting
+{
+    public class BlankCounting
+    {
+    
+        public void Main(string[] args)
+        {
+               
+        }
+        public void Count_Variable()
+        {
+            int biendem;
+            string stringtong;
+            for( int i = 0; i < stringtong.Length; i++)
+            {
+                if 
+            }
+        }
+        
+    }
+}
